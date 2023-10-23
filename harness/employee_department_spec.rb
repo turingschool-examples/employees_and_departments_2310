e79773fcb813462fb06f4a071e821aa335d6c164
Meg Stang <mstang15@gmail.com>
2023-10-23 12:54:12 -0400

    Add iteration 3 spec

--- harness/employee_department_spec.rb
+++ harness/employee_department_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './lib/employee'
 require './lib/department'
+require './lib/budget'
 
 RSpec.describe 'Employees and Departments Spec Harness' do
   before(:each) do
@@ -32,6 +33,34 @@ RSpec.describe 'Employees and Departments Spec Harness' do
   describe 'Iteration 2' do
     it '3. Department Instantiation' do
       expect(Department).to respond_to(:new).with(1).argument
+
+      expect(@customer_service).to be_a(Department)
+      expect(@customer_service).to respond_to(:name).with(0).argument
+      expect(@customer_service.name).to eq("Customer Service")
+      expect(@customer_service).to respond_to(:employees).with(0).argument
+      expect(@customer_service.employees).to eq([])
+    end
+
+    it '4. Hire Employees' do
+      expect(@customer_service).to respond_to(:hire).with(1).argument
+      @customer_service.hire(@bobbi)
+      @customer_service(@aaron)
+      expect(@customer_service.employees).to eq([@bobbi, @aaron])
+    end
+
+    it '5. Track Expenses' do
+      expect(@customer_service).to respond_to(:expenses).with(0).argument
+      expect(@customer_service.expenses).to eq(0)
+      expect(@customer_service).to respond_to(:expense).with(1).argument
+      @customer_service.expense(100)
+      @customer_service.expense(25)
+      expect(@customer_service.expenses).to eq(125)
+    end
+  end
+
+  describe 'Iteration 3' do
+    it '3. Budget Instantiation' do
+      expect(Budget).to respond_to(:new).with(1).argument
 
       expect(@customer_service).to be_a(Department)
       expect(@customer_service).to respond_to(:name).with(0).argument
