7419bc1bb44562531608cb884f3de3455eb0eaa7
Sam Puttman <daputter2@gmail.com>
2023-10-25 12:06:06 -0400

    add functionality to department

--- lib/department.rb
+++ lib/department.rb
@@ -4,5 +4,18 @@ class Department
 
   def initialize(department)
     @department = department
+    @employees = []
+  end
+
+  def name
+    @department
+  end
+
+  def employees
+    @employees
+  end
+
+  def hire(employee)
+    @employees << employee
   end
 end
--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe Employee do
     customer_service = Department.new("Customer Service")    
 
     expect(customer_service).to be_an_instance_of(Department)
+    expect(customer_service.name).to eq("Customer Service")
+    expect(customer_service.employees).to eq([])
+  end
+
+  it 'can add employees' do
+    customer_service = Department.new("Customer Service") 
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+
+    expect(customer_service.employees).not_to eq([])
   end
 end
