9fa7d69e7c12c80c3fa002821a2e1f65e390837c
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-25 11:36:08 -0400

    Initialize and test info through pry before first method

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,11 @@
 class Employee
 
-  def initialize
+  attr_reader :name, :age, :salary
+
+  def initialize(employee_data)
+    @name = employee_data[:name]
+    @age = employee_data[:age]
+    @salary = employee_data[:salary]
   end
 
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,7 +3,7 @@ require './lib/employee'
 RSpec.describe Employee do
 
   it 'can initialize' do
-    @employee = Employee.new
+    @employee = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
 
     expect(@employee).to be_an_instance_of(Employee)
   end
