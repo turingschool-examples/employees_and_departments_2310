6fabeb91ee94f1dde69aaab6c63f87a07e6937c0
Dylan Perry <dylanbperry@gmail.com>
2023-10-25 08:38:08 -0700

    Add @employees variable and test for empty array upon intialization

--- lib/department.rb
+++ lib/department.rb
@@ -1,7 +1,8 @@
 class Department
-    attr_reader :name
+    attr_reader :name, :employees
 
     def initialize(name)
         @name = name
+        @employees = []
     end
 end
--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -2,15 +2,24 @@ require "./lib/employee"
 require "./lib/department"
 
 describe Department do
-    it "exists" do
-        customer_service = Department.new("Customer Service")
 
-        expect(customer_service).to be_a Department
-    end
+    describe "#initialize" do
+        it "exists" do
+            customer_service = Department.new("Customer Service")
 
-    it "instantiates a department name from argument" do
-        customer_service = Department.new("Customer Service")
+            expect(customer_service).to be_a Department
+        end
 
-        expect(customer_service.name).to eq("Customer Service")
+        it "instantiates a department name from argument" do
+            customer_service = Department.new("Customer Service")
+
+            expect(customer_service.name).to eq("Customer Service")
+        end
+
+        it "instantiates an empty @employees variable" do
+            customer_service = Department.new("Customer Service")
+
+            expect(customer_service.employees).to eq([])
+        end
     end
 end
