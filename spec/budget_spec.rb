5d08067598594dd1b89a6cfecdcbfc26bdf884cb
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-10-25 10:00:30 -0600

    Add some initial tests for budget class

---
+++ spec/budget_spec.rb
@@ -0,0 +1,27 @@
+require './lib/department'
+require './lib/employee'
+require '.lib/budget'
+
+require "pry"
+
+RSpec.describe Department do
+  it "exists" do
+    budget_2023 = Budget.new("2023")
+    expect(budget_2023).to be_a Budget
+  end
+
+  it "has a year" do
+    budget_2023 = Budget.new("2023")
+    expect(budget_2023.year).to eq("2023")
+  end
+
+  it "has departments" do
+    budget_2023 = Budget.new("2023")
+    expect(budget_2023.departments).to eq([])
+  end
+
+  
+
+
+
+end
