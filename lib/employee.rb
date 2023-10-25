4ef80ef4b5c71b0d16ce48ae055583fd78218a1e
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-25 10:59:54 -0500

    added to has readable attributes test/updated init

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,10 +2,10 @@ class Employee
   attr_reader :name,
               :age,
               :salary
-              
+
   def initialize(data)
     @name = data[:name]
-    @age = data[:age]
-    @salary = data[:salary]
+    @age = data[:age].to_i
+    @salary = data[:salary].to_i
   end
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Employee do
   end
 
   it "has readable attributes" do
-    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
 
     expect(bobbi.name).to eq("Bobbi Jaeger")
+    expect(bobbi.age).to eq(30)
+    expect(bobbi.salary).to eq(100000)
   end
 end
