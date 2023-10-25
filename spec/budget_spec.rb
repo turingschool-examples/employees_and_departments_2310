e72792c21366a0304c86a8b86d5ba55f35b4299f
Meg Roth <megroth2@gmail.com>
2023-10-25 10:40:01 -0600

    add spec for departments wtih low expenses method in the budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -25,4 +25,19 @@ RSpec.describe Budget do
     expect(budget.departments.last.name).to eq("Finance")
   end
 
+  it 'returns departments with low expenses' do
+    budget = Budget.new(2023)
+    customer_service = Department.new("Customer Service")
+    finance = Department.new("Finance")
+    human_relations = Department.new("Human Relations")
+    budget.add_department(customer_service)
+    budget.add_department(finance)
+    budget.add_department(human_relations)
+    customer_service.expense(600)
+    finance.expense(400)
+    human_relations.expense(200)
+
+    expect(budget.departments_with_low_expenses).to eq([finance, human_relations])
+  end
+
 end
