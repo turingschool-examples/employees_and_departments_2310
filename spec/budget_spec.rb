979eae3e04a5585c19089f6d50b1fe0c215b25ac
Meg Roth <megroth2@gmail.com>
2023-10-25 10:17:54 -0600

    add spec for initialize method in budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 RSpec.describe Budget do
 
+  it 'exists' do
+    customer_service = Department.new("Customer Service")
+    finance = Department.new("Finance")
+    budget = Budget.new(2023)
+
+    expect(budget).to be_instance_of(Budget)
+    expect(budget.year).to eq(2023)
+  end
+
 end
