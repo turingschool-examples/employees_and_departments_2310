fad22a58cc3fb310b47665c813311774ed51cbc1
Meg Roth <megroth2@gmail.com>
2023-10-25 10:25:01 -0600

    add add department method to budget class

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,9 +1,13 @@
 class Budget
-  attr_reader :year
+  attr_reader :year, :departments
 
   def initialize(year)
     @year = year
     @departments = []
+  end
+
+  def add_department(department)
+    @departments << department
   end
 
 end
