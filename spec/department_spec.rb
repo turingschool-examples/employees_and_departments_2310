fd51f5af6376f81fb1a039dac67e1218565ad61a
Meg Roth <megroth2@gmail.com>
2023-10-25 10:09:31 -0600

    add spec for hire method in department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Department do
     expect(customer_service.employees).to eq([])
   end
 
+  it 'hires employee' do
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+
+    expect(customer_service.employees).to be_instance_of(Array)
+    expect(customer_service.employees.first).to be_instance_of(Employee)
+  end
+
 end
