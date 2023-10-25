56be277fefb16727c45d34d8408a23c4f5db2a53
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-25 12:03:31 -0500

    add it exists test and create budget class

---
+++ lib/budget.rb
@@ -0,0 +1,5 @@
+class Budget
+  def initialize
+    
+  end
+end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -1,2 +1,11 @@
 require './lib/department'
 require './lib/employee'
+require './lib/budget'
+
+RSpec.describe Budget do
+  it "exists" do
+    budget = Budget.new
+
+    expect(budget).to be_a(Budget)
+  end
+end
