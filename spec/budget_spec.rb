993499bc0fc7e3dc42199232bbb9c8f725e198ae
Dylan Perry <dylanbperry@gmail.com>
2023-10-25 08:55:47 -0700

    Add @year variable and test

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -5,9 +5,15 @@ require "./lib/budget"
 describe Budget do
     describe "#initialize" do
         it "exists" do
-            budget = Budget.new
+            budget = Budget.new(1912)
 
             expect(budget).to be_a Budget
+        end
+
+        it "has a year" do
+            budget = Budget.new(1912)
+
+            expect(budget.year).to eq(1912)
         end
     end
 end
