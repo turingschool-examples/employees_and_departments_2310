8aaf2bb3b17043a30aceab82972e8f22e7a2f716
Dylan Perry <dylanbperry@gmail.com>
2023-10-25 08:23:18 -0700

    Add test for initialization to ensure variables are instantiating accurately

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -6,4 +6,12 @@ describe Employee do
 
         expect(bobbi).to be_a Employee
     end
+
+    it "instantiates employee name, age, and salary" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+        expect(bobbi.name).to eq("Bobbi Jaeger")
+        expect(bobbi.age).to eq("30")
+        expect(bobbi.salary).to eq("$100000")
+    end
 end
