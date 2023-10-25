e7e7b43de1e4f684c6e72785d2747f8c787c9e29
Sam Puttman <daputter2@gmail.com>
2023-10-25 12:53:17 -0400

    add additional functionality to budget and budget spec

--- lib/budget.rb
+++ lib/budget.rb
@@ -13,4 +13,16 @@ class Budget
     @departments << department
   end
 
+  def departments_with_low_expenses
+    departments_with_min_budget = @departments.select { |department| department.expenses <= 500}
+    return departments_with_min_budget
+  end
+
+  def employee_salaries
+    salaries = []
+
+    @departments.each do |department|
+    end
+  end
+
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -14,10 +14,44 @@ RSpec.describe Budget do
   it 'can add departments' do
     budget_2023 = Budget.new(2023)
     customer_service = Department.new("Customer Service")
-    binding.pry
+
     expect(budget_2023).to be_an_instance_of(Budget)
     expect(budget_2023.add_department(customer_service)) 
   end
 
+  it 'can find low expense departments' do
+    budget_2023 = Budget.new(2023)
+    customer_service = Department.new("Customer Service")
+    quality = Department.new("Quality")
 
+    customer_service.expense(250)
+    quality.expense(5000)
+
+    budget_2023.add_department(customer_service)
+    budget_2023.add_department(quality)
+
+    expect(budget_2023.departments_with_low_expenses).not_to eq([])
+  end
+
+  it 'can add employees' do
+    budget_2023 = Budget.new(2023)
+    customer_service = Department.new("Customer Service")
+    quality = Department.new("Quality")
+
+    budget_2023.add_department(customer_service)
+    budget_2023.add_department(quality)
+
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+
+    customer_service.expense(250)
+    quality.expense(5000)
+
+    expect(budget_2023.departments.first.employees).not_to be_empty
+    expect(budget_2023.departments[1].employees).to eq([])
+
+  end
 end
