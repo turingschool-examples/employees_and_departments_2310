a142e427c50ff8adf34159a72524008fb5943bfe
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-25 11:14:28 -0500

    add exists test to Department

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -1,2 +1,10 @@
 require './lib/department'
 require './lib/employee'
+
+RSpec.describe Department do
+   it "exists" do
+    customer_service = Department.new("Customer Service") 
+
+    expect(customer_service).to be_a(Department)
+   end
+end
