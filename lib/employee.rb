10e372a91fe6a11ca215bc1c665b8171b3f0da16
Mel Langhoff <mlanghoff@uwalumni.com>
2023-10-25 09:26:41 -0600

    Employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,9 +1,16 @@
 class Employee
-    attr_reader name:, age:, salary:
+    attr_reader :name, :age, :salary, :amount_of_raise
 
     def initialize(employee)
         @name = employee[:name]
         @age = employee[:age]
         @salary = employee[:salary]
     end
+
+    def give raise(amount_of_raise)
+
+
+    end
+
+
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe Employee do
         
         expect(bobbi).to be_a Employee
     end
+
+    it "has a name" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+        
+        expect(bobbi.name).to be("Bobbi Jaeger")
+    end
+
+    it "has an age" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+        expect(bobbi.age).to be("30")
+    end
+
+    it "has a salary" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+        expect(bobbi.salary).to be("$100000")
+    end
+
+
+
+
+
 end
