628a3ef2bb2b6d5212f3fc4989e12df734a79555
Meg Roth <megroth2@gmail.com>
2023-10-25 09:59:47 -0600

    add give raise method to employee class and update spec

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,10 +1,14 @@
 class Employee
-  attr_reader :name, :age, :salary
+  attr_reader :name, :age, :salary, :employee_details
 
   def initialize(employee_details)
     @name = employee_details[:name]
     @age = employee_details[:age].to_i
     @salary = employee_details[:salary].delete_prefix("$").to_i
+  end
+
+  def give_raise(raise_amount)
+    @salary += raise_amount
   end
 
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -5,15 +5,14 @@ RSpec.describe Employee do
   it 'exists' do
     bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
 
-    binding.pry
-
     expect(bobbi).to be_instance_of(Employee)
     expect(bobbi.name).to eq("Bobbi Jaeger")
-    expect(bobbi.age).to eq("30")
-    expect(bobbi.salary).to eq("$100000")
+    expect(bobbi.age).to eq(30)
+    expect(bobbi.salary).to eq(100000)
   end
 
   it 'gives raise' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
     bobbi.give_raise(5000)
 
     expect(bobbi.salary).to eq(105000)
