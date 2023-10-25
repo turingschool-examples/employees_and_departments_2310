49fcf22e51d53ee01f7a797130f0954fd2295ac2
Dylan Perry <dylanbperry@gmail.com>
2023-10-25 08:35:49 -0700

    Add @name variable and test for argument instantiation

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -7,4 +7,10 @@ describe Department do
 
         expect(customer_service).to be_a Department
     end
+
+    it "instantiates a department name from argument" do
+        customer_service = Department.new("Customer Service")
+
+        expect(customer_service.name).to eq("Customer Service")
+    end
 end
