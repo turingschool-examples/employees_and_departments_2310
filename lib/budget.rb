ca72cb036042717c767f6795ce82cfcb6d763b9d
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-25 13:01:12 -0400

    feat: Add year method

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,11 +1,17 @@
 class Budget
 
-  attr_reader :year, :department
+  attr_reader :year, :departments
 
   def initialize
-    @year = year
+    @year = 0
     @departments = []
-
   end
+
+  def year
+    @year = 2023
+  end
+
+  #def add_department(department)
+
 
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -2,13 +2,19 @@ require './lib/department'
 require './lib/employee'
 require './lib/budget'
 
-
 RSpec.describe Budget do
 
   it 'can initialize' do
     budget = Budget.new
 
     expect(budget).to be_an_instance_of(Budget)
+  end
+
+  #not quite, but works
+  it 'can set the year' do
+    budget = Budget.new
+
+    expect(budget.year).to eq(2023)
   end
 
 
