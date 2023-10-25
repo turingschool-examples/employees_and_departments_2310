ff177d723d24104f090c7a37e2ab7ae3e623d2ce
Meg Roth <megroth2@gmail.com>
2023-10-25 10:24:43 -0600

    add spec for add department method in budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -3,12 +3,25 @@ require 'spec_helper'
 RSpec.describe Budget do
 
   it 'exists' do
-    customer_service = Department.new("Customer Service")
-    finance = Department.new("Finance")
     budget = Budget.new(2023)
 
     expect(budget).to be_instance_of(Budget)
     expect(budget.year).to eq(2023)
+  end
+
+  it 'adds departments' do
+    budget = Budget.new(2023)
+    customer_service = Department.new("Customer Service")
+    budget.add_department(customer_service)
+
+    expect(budget.departments).to be_instance_of(Array)
+    expect(budget.departments).not_to be(nil)
+    expect(budget.departments.first.name).to eq("Customer Service")
+
+    finance = Department.new("Finance")
+    budget.add_department(finance)
+
+    expect(budget.departments.last.name).to eq("Finance")
   end
 
 end
