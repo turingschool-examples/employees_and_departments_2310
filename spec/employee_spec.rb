b85bbf56cca510ab0fe0df5e67ff3426ac85b4c4
Barry Allahyar <barrya@gmail.com>
2023-10-25 11:56:30 -0500

    test: has a salary

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -14,5 +14,16 @@ RSpec.describe Employee do
     expect(bobbi.name).to eq("Bobbi Jaeger")
   end
 
-  
+  it 'has an age' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi.age).to eq("30")
+  end
+
+  it 'has a salary' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi.salary).to eq("100000")
+  end
+
 end
