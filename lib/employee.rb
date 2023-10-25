d48ecfb23c9f9f0c1b5c48cce9102780f4bd9bf7
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:29:04 -0700

    employee raise and initialize fix (initialize spelt wrong and change way to remove dollar sign from string

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,10 +1,10 @@
 class Employee
     attr_reader :name, :age, :salary
 
-    def initalize(input_info)
+    def initialize(input_info)
         @name = input_info[:name]
         @age = input_info[:age].to_i
-        @salary = input_info[:salary].slice!(0).to_i
+        @salary = input_info[:salary][1..-1].to_i
     end
 
     def give_raise(raise_ammount)
