e457534d873287903f2b4c3d2cc32146f93e9f8e
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:53:33 -0700

    budget initialize method and test

--- lib/budget.rb
+++ lib/budget.rb
@@ -2,8 +2,11 @@ require './lib/employee'
 require './lib/department'
 
 class Budget
-    attr_reader
+    attr_reader :year, :departments
+    #           :int   :array
 
-    def initialize()
+    def initialize(year_input)
+        @year = year_input
+        @departments = []
     end
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -3,9 +3,10 @@ require './lib/department'
 require './lib/budget'
 
 RSpec.describe Department do
-    it "can initialize new budget" do
-
+    it "can initialize new Budget" do
+        budget = Budget.new(2023)
+        expect(budget.year).to eq(2023)
+        expect(budget.departments).to eq([])
     end
 
-    expect(customer_service.employees).to eq([bobbi, aaron])
 end
