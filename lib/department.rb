90d9e7895244d4a5c5c74189c172987947dd436a
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:45:03 -0700

    department lib iteration 2 complete

--- lib/department.rb
+++ lib/department.rb
@@ -1,3 +1,5 @@
+require './lib/employee'
+
 class Department
     attr_reader :employees, :expenses, :name
 
@@ -5,5 +7,13 @@ class Department
         @name = name_input
         @employees = []
         @expenses = 0
+    end
+
+    def hire(employee)
+        @employees.append(employee)
+    end
+
+    def expense(expense_input)
+        @expenses += expense_input
     end
 end
