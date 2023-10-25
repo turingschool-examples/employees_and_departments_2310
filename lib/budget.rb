d95a138cf10b149a517eca6ccfbe3aa3d86766bb
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:48:28 -0700

    budget initial commit

---
+++ lib/budget.rb
@@ -0,0 +1,9 @@
+require './lib/employee'
+require './lib/department'
+
+class Budget
+    attr_reader
+
+    def initialize()
+    end
+end
---
+++ spec/budget_spec.rb
@@ -0,0 +1,11 @@
+require './lib/employee'
+require './lib/department'
+require './lib/budget'
+
+RSpec.describe Department do
+    it "can initialize new budget" do
+
+    end
+
+    expect(customer_service.employees).to eq([bobbi, aaron])
+end
