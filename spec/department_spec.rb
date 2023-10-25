068d35cafd5b274412c7e023083fa5824d62dedd
Meg Roth <megroth2@gmail.com>
2023-10-25 10:13:04 -0600

    add spec for expense method in department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Department do
     expect(customer_service).to be_instance_of(Department)
     expect(customer_service.name).to eq("Customer Service")
     expect(customer_service.employees).to eq([])
+    expect(customer_service.expenses).to eq(0)
   end
 
   it 'hires employee' do
@@ -19,6 +20,14 @@ RSpec.describe Department do
 
     expect(customer_service.employees).to be_instance_of(Array)
     expect(customer_service.employees.first).to be_instance_of(Employee)
+  end
+
+  it 'expenses amounts' do
+    customer_service = Department.new("Customer Service")
+    customer_service.expense(100)
+    customer_service.expense(25)  
+
+    expect(customer_service.expenses).to eq(125)
   end
 
 end
