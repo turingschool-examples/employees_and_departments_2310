c53550155b6a839e9411d7bac461b642327490aa
Martin Chavez <martinchavez1203@gmail.com>
2023-10-25 10:45:29 -0600

    add test for add_departments in budget spec

--- lib/budget.rb
+++ lib/budget.rb
@@ -4,4 +4,5 @@ class Budget
     @year = year 
     @departments = []
   end
+  
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -5,6 +5,9 @@ require './lib/budget'
 RSpec.describe Budget do
   before(:each) do
     @budget_1 =Budget.new(2023)
+    @department_1 = Department.new("Customer Service") 
+    @department_2 = Department.new("Seasonal") 
+    @department_3 = Department.new("HR") 
   end
 
   describe '#initialize' do
@@ -17,6 +20,16 @@ RSpec.describe Budget do
     it 'reads attributes' do
       expect(@budget_1.year).to eq(2023)
       expect(@budget_1.departments).to eq([])
+    end
+  end
+
+  describe '#add_departments' do
+    it 'adds departments to the budget' do
+      expect(@budget_1.departments).to eq([])
+      @budget_1.add_departments(@department_1)
+      @budget_1.add_departments(@department_2)
+      expect(@budget_1.departments).to eq([@department_1,@department_2])
+      
     end
   end
 end
