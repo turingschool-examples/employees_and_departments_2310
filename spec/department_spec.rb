62d368d75fe7778ea7762ea8c6ff65fa799d5112
Meg Roth <megroth2@gmail.com>
2023-10-25 10:03:06 -0600

    add spec for initialize method in department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Department do
 
+  it 'exists' do
+    customer_service = Department.new("Customer Service")
+
+    expect(customer_service).to be_instance_of(Department)
+    expect(customer_service.name).to eq("Customer Service")
+    expect(customer_service.employees).to eq([])
+  end
+
 end
