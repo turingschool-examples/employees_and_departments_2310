66464ac2cfe6f66fdbecfe26edf7214a5cccf623
Barry Allahyar <barrya@gmail.com>
2023-10-25 11:20:16 -0500

    test: employee class can intialize test

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -2,3 +2,10 @@ require 'pry'
 require './lib/department'
 require './lib/employee'
 
+RSpec.describe Employee do
+  it 'can intialize' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi).to be_a Employee
+  end
+end
