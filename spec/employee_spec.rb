f686a2cba27baaf9d966d959f699e67736b8307a
Meg Roth <megroth2@gmail.com>
2023-10-25 09:22:24 -0600

    add spec for initialize method in employee class

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 RSpec.describe Employee do
 
+  it 'exists' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi).to be_instance_of(Employee)
+    expect(bobbi.name).to eq("Bobbi Jaeger")
+    expect(bobbi.age).to eq(30)
+    expect(bobbi.salary).to eq($100000)
+  end
+
 end
