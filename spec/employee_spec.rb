affb7bcc50e6bd6d3a5b22dd371e45391cdfff9e
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:20:00 -0700

    employee test function setup

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,20 +1,19 @@
+
 pry(main)> require './lib/employee'
-# => true
 
-pry(main)> bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
-# => #<Employee:0x00007fdfd48af848...>
+RSpec.describe Employee do
+    it "can initalize new employee" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
 
-pry(main)> bobbi.name
-# => "Bobbi Jaeger"
+        expect(bobbi.name).to eq("Bobbi Jaeger")
+        expect(bobbi.age).to eq(30)
+        expect(bobbi.salary).to eq(100000)
+    end
 
-pry(main)> bobbi.age
-# => 30
-
-pry(main)> bobbi.salary
-# => 100000
-
-pry(main)> bobbi.give_raise(5000)
-# => #<Employee:0x00007fce46194788...>
-
-pry(main)> bobbi.salary
-# => 105000
+    it "can give raises" do
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+        bobbi.give_raise(5000)
+        
+        expect(bobbi.salary).to eq(105000)
+    end
+end
