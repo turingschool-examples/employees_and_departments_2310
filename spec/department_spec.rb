c58a8d2d1433db1c58f85392e2287d50095f1547
Quin Nordmark <quinnordmark@gmail.com>
2023-10-25 09:40:09 -0700

    department test, probably complete

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -2,38 +2,32 @@ require './lib/employee'
 require './lib/department'
 
 RSpec.describe Department do
-    it "" do
-        
+    it "can initialize new Department" do
+        customer_service = Department.new("Customer Service")
+
+        expect(customer_service.name).to eq("Customer Service")
+        expect(customer_service.employees).to eq([])
+        expect(customer_service.expenses).to eq(0)
     end
-    customer_service = Department.new("Customer Service")    
-# => #<Department:0x00007fce46993d58...>
 
-    customer_service.name
-# => "Customer Service"
+    it "can add new Employees" do
+        customer_service = Department.new("Customer Service")
+        bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+        aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+        customer_service.hire(bobbi)
+        customer_service.hire(aaron) 
 
-    customer_service.employees
-# => []
+        expect(customer_service.employees).to eq([bobbi, aaron])
+    end
 
-    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
-# => #<Employee:0x00007fce46194788...>
+    it "can add new expenses" do
+        customer_service = Department.new("Customer Service")
+        expect(customer_service.expenses).to eq(0)
 
-    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
-# => #<Employee:0x00007fce46a610a0...>
+        customer_service.expense(100)
+        expect(customer_service.expenses).to eq(100)
 
-    customer_service.hire(bobbi)
-
-    customer_service.hire(aaron)    
-
-    customer_service.employees
-# => [#<Employee:0x00007fce46194788...>, #<Employee:0x00007fce46a610a0...>]
-
-    customer_service.expenses
-# => 0
-
-    customer_service.expense(100)
-
-    customer_service.expense(25)    
-
-    customer_service.expenses
-# => 125
+        customer_service.expense(25)
+        expect(customer_service.expenses).to eq(125)
+    end
 end
