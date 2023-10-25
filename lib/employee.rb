bb9cc66ff00d6a4ec268e5c5e1da8e7654f1f783
Mel Langhoff <mlanghoff@uwalumni.com>
2023-10-25 09:19:23 -0600

    Employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,11 +1,9 @@
 class Employee
     attr_reader name:, age:, salary:
 
-    def initialize
-        @employee = {
-            name:
-            age:
-            salary:
-        }
+    def initialize(employee)
+        @name = employee[:name]
+        @age = employee[:age]
+        @salary = employee[:salary]
     end
 end
